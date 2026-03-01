f913f9364b7a48bf1e59d8079bba0b44ad13c076
mitoha-dev <mitoha.dev@outlook.jp>
2026-03-01 21:27:22 +0900

    ex:rustlings演習

--- rustlings/exercises/06_move_semantics/move_semantics3.rs
+++ rustlings/exercises/06_move_semantics/move_semantics3.rs
@@ -1,5 +1,6 @@
 // TODO: Fix the compiler error in the function without adding any new line.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
     vec.push(88);
 
     vec
@@ -16,7 +17,7 @@ mod tests {
     #[test]
     fn move_semantics3() {
         let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
         assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
