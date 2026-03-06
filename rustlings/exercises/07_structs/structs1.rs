8c8d20679d2fdbda61fef28fa7f0d6ff13a53ad6
mitoha-dev <mitoha.dev@outlook.jp>
2026-03-06 17:32:45 +0900

    ex:rustlingsの練習

--- rustlings/exercises/07_structs/structs1.rs
+++ rustlings/exercises/07_structs/structs1.rs
@@ -1,9 +1,10 @@
 struct ColorRegularStruct {
-    // TODO: Add the fields that the test `regular_structs` expects.
-    // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red: i64,
+    green: i64,
+    blue: i64,
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+struct ColorTupleStruct(i64, i64, i64);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -20,7 +21,11 @@ mod tests {
     fn regular_structs() {
         // TODO: Instantiate a regular struct.
         // let green =
-
+        let green = ColorRegularStruct {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
         assert_eq!(green.blue, 0);
@@ -31,6 +36,8 @@ mod tests {
         // TODO: Instantiate a tuple struct.
         // let green =
 
+        let green = ColorTupleStruct(0, 255, 0);
+
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -40,6 +47,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
         // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
